b5b590c82c5085219be644f9348d0a558d4c8565
muhammadali224 <muhammad.alfaqoui@gmail.com>
2024-05-18 01:45:52 +0300

    add animation to splash screen

--- lib/features/splash/presentation/views/widgets/splash_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_body.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_book/features/home/presentation/views/home.dart';
 import 'package:bloc_book/generated/assets.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:get/route_manager.dart';
 
 class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
@@ -22,6 +24,11 @@ class _SplashBodyState extends State<SplashBody>
         Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
             .animate(animationController);
     animationController.forward();
+    Future.delayed(
+        const Duration(seconds: 2),
+        () => Get.to(() => const HomeScreen(),
+            transition: Transition.fadeIn,
+            duration: const Duration(milliseconds: 250)));
   }
 
   @override
